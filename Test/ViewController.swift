77978b6692d02671116c443e09faa2d4a205f71d
tylerliu <tylersiqi@163.com>
2019-03-30 22:48:16 -0700

    Get Rocking

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -28,11 +28,46 @@ UINavigationControllerDelegate {
     
     
 }
+
+func recommandMusic(image:UIImage){
+    var imageData: Data?
+    imageData = image.jpegData(compressionQuality: 0.5)
+    let url = URL(string: "http://45.33.47.131:3000/image")!
+    let json: [String: String] = ["image": imageData!.base64EncodedString()]
+    let jsonData = try? JSONSerialization.data(withJSONObject: json)
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.httpBody = jsonData
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data, error == nil else {
+            print(error?.localizedDescription ?? "No data")
+            return
+        }
+        var responseURL = String(data: data, encoding: .utf8)!
+        responseURL = String(responseURL.prefix(responseURL.count - 1))
+        
+        print(responseURL)
+        guard let url = URL(string: responseURL) else {
+            print("Error");
+            return;
+        }
+        DispatchQueue.main.async {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+        
+    }
+    
+    task.resume()
+}
+
 extension ViewController{
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let img = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             myImage.image = img
+            recommandMusic(image: img)
         }
         dismiss(animated: true, completion: nil)
     }
