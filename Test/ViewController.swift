f9073d8646dbd29d47bd5907df762b08e62caa5b
Ahren09 <1099052927@qq.com>
2019-03-30 23:38:58 -0700

    Fix Button and picture view

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -18,6 +18,8 @@ UINavigationControllerDelegate {
         super.viewDidLoad()
         imagePicker.delegate=self
         // Do any additional setup after loading the view.
+        let image1 : UIImageView = self.view.viewWithTag(200) as! UIImageView;
+        image1.image = UIImage(named:"bg.jpg");
     }
 
     @IBAction func Photo(_ sender: Any) {
@@ -33,6 +35,11 @@ extension ViewController{
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let img = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             myImage.image = img
+            let label1 : UILabel = self.view.viewWithTag(300) as! UILabel;
+            label1.text = "Image Loaded";
+            let button1 : UIButton = self.view.viewWithTag(100) as! UIButton;
+            view.bringSubviewToFront(button1);
+            view.bringSubviewToFront(label1);
         }
         dismiss(animated: true, completion: nil)
     }
